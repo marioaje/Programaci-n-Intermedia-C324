32b371cd6af5f905ca712fc9d1e8044342b7e3f4
marioaje <marioaje@gmail.com>
2024-10-01 19:28:35 -0600

    Update ejercicio1.py

--- Semana 3/ejercicio1.py
+++ Semana 3/ejercicio1.py
@@ -8,3 +8,28 @@
 # El promedio de las calificaciones.
 # La calificación más alta.
 # La calificación más baja.
+
+
+#Lista de Calificaciones
+calificaciones = []
+
+# Pide al usuario ingresar las calificaciones de 5 estudiantes.
+for i in range(5):    
+    valorcapturado =  float(input( f"Introduzca la nueva calificacion del estudiante: { i + 1 }: "))
+    calificaciones.append(valorcapturado)
+    
+# Calcula y muestra:
+# El promedio de las calificaciones.
+# La calificación más alta.
+# La calificación más baja.
+print (calificaciones)
+
+# El promedio de las calificaciones.
+promedio = sum(calificaciones) / len(calificaciones)
+print (f"El promedio: {promedio}")
+# La calificación más alta.
+calificacion_maxima = max(calificaciones)
+print (f"La calificación más alta: {calificacion_maxima}")
+# La calificación más baja.
+calificacion_minima = min(calificaciones)
+print (f"La calificación más baja: {calificacion_minima}")
