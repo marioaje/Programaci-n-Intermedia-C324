aa03e4087ebac5fe54f44d16b8e1f423d347f637
marioaje <marioaje@gmail.com>
2024-10-01 18:43:44 -0600

    Create ejemplo.py
    Ejemplo de la clase

---
+++ Semana 3/ejemplo.py
@@ -0,0 +1,28 @@
+print("Mi nombre: Mario")
+
+
+# Declaracion de variables
+nombre = "Mario"
+edad = 40
+pi = 3.1416
+esreal = True
+
+print( nombre )
+print( edad )
+print( pi )
+print( esreal )
+
+#operaciones basicas
+a = 5
+b = 6
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+cuadrado = a ** b
+print( suma )
+print( resta )
+print( cuadrado )
+print( multiplicacion )
+print( division )
