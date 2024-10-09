eb3f440d2dfa2ad0d21bd6d953c56c0be477a2d8
marioaje <marioaje@gmail.com>
2024-10-08 19:57:18 -0600

    Create ejercicio1profe.py

---
+++ Semana 4/ejercicio1profe.py
@@ -0,0 +1,32 @@
+# Ejercicio 1: Diccionario de Calificaciones
+# Crea un programa que permita al usuario ingresar los nombres y calificaciones de varios estudiantes, y luego calcula el promedio de las calificaciones.
+# Instrucciones:
+# El usuario ingresa la cantidad de estudiantes.
+# Luego, el usuario ingresa el nombre y la calificación de cada estudiante.
+# El programa calcula y muestra el promedio de las calificaciones.
+# Código: En archivo separado
+
+ #Diccionario de Calificaciones
+califcaciones = {}
+ 
+continuar = "s" 
+# Pide al usuario ingresar las calificaciones de los estudiantes que necesite.
+numeroestudiantes = 0
+while continuar.lower() == "s":
+    # Luego, el usuario ingresa el nombre
+    nombrecapturado =  input( f"El nombre del estudiante: ")
+    
+    #a calificación de cada estudiante.
+    valorcapturado =  float(input( f"Introduzca la nueva calificacion del estudiante: "))
+    califcaciones[nombrecapturado] = valorcapturado
+    numeroestudiantes += 1
+    continuar = input( "Desea ingresar un nuevo estudiante (s/n)" )
+
+
+# El promedio de las calificaciones.
+promedio = sum(califcaciones.values()) / numeroestudiantes
+
+print(califcaciones)
+print("El promedio es:", promedio)
+print("Cantidad de estudiantes:", numeroestudiantes)
+
