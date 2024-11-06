54eef2a2daba7db31cef9702e4731b192e7a749f
marioaje <marioaje@gmail.com>
2024-11-05 18:13:44 -0600

    semana 8

---
+++ Semana 8/funcionesmatematicas.py
@@ -0,0 +1,13 @@
+#Redactamos funciones procesos...
+def acasumo(a,  b):
+    return a + b;
+
+def acaresta(a,  b):
+    return a - b;
+
+def acamultiplico(a,  b):
+    return a * b;
+
+
+def acadivido(a,  b):
+    return a / b;
---
+++ Semana 8/unarchivonormal.py
@@ -0,0 +1,22 @@
+
+#nos permite traer modulos y ccomponentes para las funciones personalizados
+import funcionesmatematicas
+#from funcionesmatematicas import acasumo 
+
+print("Iniciando system")
+
+resultado = funcionesmatematicas.acasumo(8, 10)
+print(resultado)
+
+resultado = funcionesmatematicas.acaresta(8, 10)
+print(resultado)
+
+resultado = funcionesmatematicas.acamultiplico(8, 10)
+print(resultado)
+
+resultado = funcionesmatematicas.acadivido(8, 10)
+print(resultado)
+
+
+# resultado = acasumo(8, 10)
+# print(resultado)
