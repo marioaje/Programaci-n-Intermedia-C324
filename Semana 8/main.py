f73be083f3020b3f5a08c01da6fc00146f0c28cc
marioaje <marioaje@gmail.com>
2024-11-05 18:28:57 -0600

    ejercicio

---
+++ Semana 8/main.py
@@ -0,0 +1,24 @@
+# Ejercicio: Gestión de Inventario
+# Imagina que eres el encargado de una pequeña tienda y necesitas un programa que te ayude a gestionar el inventario. Este programa debe permitir agregar productos, consultar información, actualizar el stock y eliminar productos del inventario.
+
+# Requerimientos:
+# Agregar producto: Permite agregar un nuevo producto al inventario. Debe incluir:
+
+# Un id_producto único.
+# Un nombre.
+# Un precio.
+# La cantidad en stock.
+# Consultar producto: Permite consultar un producto específico en el inventario por su id_producto y mostrar todos sus datos.
+
+# Actualizar stock: Permite actualizar la cantidad en stock de un producto específico.
+
+# Eliminar producto: Permite eliminar un producto del inventario.
+
+# Mostrar inventario: Imprime todos los productos disponibles en el inventario.
+
+# Instrucciones
+# Crea una función para cada requerimiento, por ejemplo: agregar_producto, consultar_producto, actualizar_stock, eliminar_producto, y mostrar_inventario.
+
+# Usa un diccionario llamado inventario para almacenar la información de los productos, donde las claves sean id_producto y los valores contengan otro diccionario con los detalles de cada producto.
+
+# Utiliza un menú para que el usuario pueda seleccionar la acción que desea realizar.
